14cde0b7f3bb72c884fab9e9b2294a27b60feef8
cmolarti <cmolarti@gmail.com>
2023-11-27 00:20:53 -0500

    Add the Exit Buttton

--- gui.py
+++ gui.py
@@ -11,9 +11,10 @@ if __name__ == '__main__':
     lbl = gui.Text('Type in a ToDo:')
     in_box = gui.InputText(tooltip='Enter a to-do')
     btnAdd = gui.Button('Add', target=addItem2(in_box.get()))
-    
+    btnExit = gui.Button('Exit')
 
-    win = gui.Window(f'ToDo App', layout=[[lbl], [in_box, btnAdd]])
+
+    win = gui.Window(f'ToDo App', layout=[[lbl], [in_box, btnAdd], [btnExit]])
 
     win.read()  #displays the window
     win.close()
