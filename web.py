f7fecd4ca01cb458f7fb6a1484b7b8232257695d
cmolarti <cmolarti@gmail.com>
2023-12-09 21:03:52 -0500

    add the checkbox complete

--- web.py
+++ web.py
@@ -2,18 +2,33 @@ import streamlit as st
 import modules.func as func
 
 
+
+def remove_todo():
+    for k, x in st.session_state.items():
+        if x == True:
+            func.removeItem2(k)
+
+def add_todo():
+    new_todo = st.session_state['add_new_todo']
+    func.addItem2(new_todo)
+    st.session_state['add_new_todo'] = ''
+
 if __name__ == '__main__':
     st.title('My Todo App')
     st.subheader('This is my todo app.')
     st.write('This app is to increase productivity')
 
-    #add checkbox
-    # st.checkbox('Buy grocery')
-
     func.loadfromFile()
 
     for x in func.todo:
-        st.checkbox(x)
-    
+        #add checkbox
+        st.checkbox(x, key=f'{x}', on_change=remove_todo)
+        
+
     #add text input
-    st.text_input(label='Enter a todo:', placeholder='Add a todo...')
+    st.text_input(label='Enter a todo:', placeholder='Add a todo...', 
+                on_change=add_todo, key='add_new_todo')
+    
+    
+            
+
